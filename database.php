64cd067bcfbd02b6951935259622edacfecb486d
Raymond Esteybar <resteybar@csumb.edu>
2018-04-17 04:06:10 +0000

    Changes

--- database.php
+++ database.php
@@ -19,7 +19,7 @@
            $host = $url["host"];
            $dbname   = substr($url["path"], 1);
            $username = $url["user"];
-           $password = $url["password"];
+           $password = $url["pass"];
         }
         
         // Create connection
@@ -30,9 +30,9 @@
         
         $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
         $opt = [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
         ];
         $pdo = new PDO($dsn, $username, $password, $opt);
         return $pdo; 
